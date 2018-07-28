cf9ff03704596596a395b42ccc270693b8ad6e16
Max Khamrovskyi <maksim36ua@gmail.com>
2018-07-28 15:21:32 +0300

    Add timer function to folder

--- User.cs
+++
@@ -1,7 +0,0 @@
-public class User
-{
-	public string TelegramUserId { get; set; }
-	public string TelegramNickname { get; set; }
-	public string TelegramName { get; set; }
-	public string TelegraphAccessToken { get; set; }
-}
